0cc06a028f91d8c0158cd20c7d38b5ec0b4978c3
Nicole Hart <nicole.hart@capitalone.com>
2017-04-04 20:28:20 -0400

    attempt 1

--- HomePwner/HomePwner/ItemsViewController.swift
+++ HomePwner/HomePwner/ItemsViewController.swift
@@ -18,13 +18,22 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell2 = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell2", for: indexPath)
         
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        if item.valueInDollars > Int(50) {
+            cell.textLabel?.text = item.name
+            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+            return cell
+        } else {
+            cell2.textLabel?.text = item.name
+            cell2.detailTextLabel?.text = "$\(item.valueInDollars)"
+            return cell2
+        }
         
-        return cell
+        
+        
     }
     
     override func viewDidLoad() {
